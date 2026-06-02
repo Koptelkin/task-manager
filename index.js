298c38f8f211ada8556f1f2df4678a74914c341b
Koptelkin <120393725+Koptelkin@users.noreply.github.com>
2026-06-03 01:18:32 +0300

    second practice

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-let task = 'Написать рецепт пельменей';
+const tasks = [];
+const completedTasks = [];
 let completedTaskCount = 0;
 
 function showTask() {
-    if (!task) {
-        console.log('Задача отсутствует')
-    } else {
-        console.log(task)
-    }
+    tasks.forEach((task) => {
+        console.log(`Название - ${task.title}, Описание - ${task.description}, Статус - ${task.isCompleted ? 'Выполнено' : 'Не выполнено'}, Дата создания - ${task.createdDate}, Дата выполнения - ${task.completedDate}`);
+    })
 }
 
-function setTask(taskDescription) {
-    if (!taskDescription) {
+function setTask(title, description) {
+    if (!title) {
         console.log('Введите непустое описание задачи')
         return;
     }
 
-    if (task) {
-        console.log('Не могу добавить задачу, завершите или удалите предыдущую')
-        return;
-    }
-
-    task = taskDescription;
+    tasks.push({
+        title: title, 
+        description: description, 
+        isCompleted: false, 
+        createdDate: new Date(), 
+        completedDate: null
+    })
 }
 
-const completeTask = () => {  
-     if (!task) {
-        console.log('Нет текущей задачи')
-    } else {
-        task = ''
-        completedTaskCount++
-    }
+const completeTask = (task) => {  
+    task.isCompleted = true;
+    task.completedDate = new Date();
+    completedTasks.push(task);
+    completedTaskCount++;
 }
 
-const deleteTask = () => {
-    if (!task) {
-        console.log('Нет текущей задачи')
+const deleteTask = (task) => {
+    if (!task.isCompleted) {
+        let shouldDelete = confirm('Задача ещё не выполнена, удалить?');
+        const index = tasks.indexOf(task);
+        if (shouldDelete) {
+            index > -1 ? tasks.splice(index, 1) : 'Задача не найдена в списке актуальных задач';
+
+        }
     } else {
-        task = ''
+        const completedIndex = completedTasks.indexOf(task);
+        if (completedIndex > -1) {
+            completedTasks.splice(completedIndex, 1);
+            completedTaskCount--;
+        } else {
+            return 'Задача не найдена в списке выполненных задач';
+        }
     }
+}   
+
+function clearTasks() {
+    tasks.length = 0;
 }
